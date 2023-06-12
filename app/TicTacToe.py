15635335ec7739d5473bf9754b5e2ab3ae6b1e33
UniverSapphireLotus <49215164+UniverSapphireLotus@users.noreply.github.com>
2023-06-12 14:57:26 -0400

    Update TicTacToe.py
    some test

--- app/TicTacToe.py
+++ app/TicTacToe.py
@@ -2,12 +2,18 @@
 
 class TicTacToe:
     def __init__(self, player_a = 'X', player_b = 'O', size = 3):
+        self.size = size
         self.board = [['' for _ in range(size)] for _ in range(size)]
+
         self.player_a = player_a
         self.player_b = player_b
 
+        self.turn = True # true for player_a, false for player_b
+
         self.diag_1 = [[i,i] for i in range(size)]
         self.diag_2 = [[size - i - 1,i] for i in range(size)]
+
+        self.moves = 0
         
     def print_board(self):
         print('===============')
@@ -15,47 +21,75 @@ class TicTacToe:
             print(i)
         print('===============')
     
-    def make_move(self, x, y, player):
-        self.board[x][y] = player
+    def make_move(self, x, y):
+        print('moves....', self.turn)
+        if self.is_valid_move(x,y):
+            if self.turn:
+                self.board[x][y] = self.player_a
+                self.turn = False
+            else:
+                self.board[x][y] = self.player_b
+                self.turn = True
+
+            self.moves += 1
+            return True
+        else:
+            print('Invalid move....')
+            return False
+    
+    def is_valid_move(self, x, y):
+        return True if self.board[x][y] == '' else False
 
     def check(self, set_player):
         return True if len(set_player) == 1 and set_player != {''} else False
+    
+    def is_board_full(self):
+        return True if self.moves == 9 else False
+    
+    def reset_board(self):
+        self.board = [['' for _ in range(self.size)] for _ in range(self.size)]
+
+    def empty_boxes(self):
+        return [[i,j] for i in range(self.size) for j in range(self.size) if self.board[i][j] == '']
 
     def is_winner(self, x, y, player):
-        #-----
-        print(f'[{x},{y}]')
-        print('row>', self.board[x])
-        print('col>', [i[y] for i in self.board])
-        #print('diag 1>', [i[y] for i in self.board])
 
         if self.check(set(self.board[x])):
             print('winner row', player)
+            return True
         elif self.check(set([i[y] for i in self.board])):
             print('winner col', player)
+            return True
         else:
-            print(self.diag_1)
-            print(self.diag_2)
+            #print(self.diag_1)
+            #print(self.diag_2)
             if [x, y] in self.diag_1:
                 if self.check(set([self.board[d[0]][d[1]] for d in self.diag_1])):
                     print('winner diag_1', player)
+                    return True
             elif [x, y] in self.diag_2:
                 if self.check(set([self.board[d[0]][d[1]] for d in self.diag_2])):
                     print('winner cdiag_2', player)
-            #[0,0], [1,1], [2,2]
-            #[0,2], [1,1], [2,0]
-
-    
-        
+                    return True
+        return False
 
         
-        
+print('Start....')
+print('================================================================')
+import random
+pos_ = [0,1,2]
 
 game = TicTacToe()
-game.print_board()
-game.make_move(0, 0, 'X')
-game.make_move(1, 2, 'O')
-game.make_move(1, 1, 'X')
-game.make_move(2, 2, 'O')
-game.make_move(2, 2, 'X')
-game.print_board()
-game.is_winner(2, 2, 'O')
+player_list = ['X', 'O']
+
+while not game.is_board_full() :
+    xx, xy = random.choice(pos_), random.choice(pos_)
+    game.make_move(xx, xy)
+    game.print_board()
+    if game.is_winner(xx, xy, 'O' if game.turn else 'X'):
+        print(f'winner: player_list[{not game.turn}]')
+        break
+    
+print(game.empty_boxes())
+print('================================================================')
+print('....end')
